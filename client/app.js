e3488f22bc1eadbab49361c4b1593280b2766e44
Jessica Paull <jessikwa@live.com>
2016-10-17 17:15:35 -0400

    first commit

---
+++ client/app.js
@@ -0,0 +1,26 @@
+Template.body.helpers({
+  
+  polls: function() {
+    return Polls.find();
+  }
+  
+});
+
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
+
+
+Template.poll.helpers({
+  choices: function() {
+    var coll = Polls.findOne(this._id);
+    return _.sortBy(coll.choices, function(choice) {
+      return -choice.votes;
+    });
+  }
+});
---
+++ client/components/poll.js
@@ -0,0 +1,53 @@
+// attach events to our poll template
+Template.poll.events({
+
+  // event to handle clicking a choice
+  'click .vote': function(event) {
+
+    // prevent the default behavior
+    event.preventDefault();
+  
+    // get the parent (poll) id
+    var pollID = $(event.currentTarget).parent('.poll').data('id');
+    var voteID = $(event.currentTarget).data('id');
+
+    // create the incrementing object so we can add to the corresponding vote
+    var voteString = '$.choices.id.' + voteID + '.votes';
+    var action = {};
+    action[voteString] = 1;
+    
+    // increment the number of votes for this choice
+    Polls.update(
+      { _id: pollID }, 
+      { $inc: action }
+    );
+
+  },
+
+  // event to handle adding a person
+  'click .add-person': function(event) {
+
+    // prevent the default behavior
+    event.preventDefault();
+  
+    // get the parent (poll) id
+    var pollID = $(event.currentTarget).closest('.poll').data('id');
+
+    // get the text for the new choice
+    var input = $('input.new-person');
+    var name = input.val();
+    input.val("");
+
+    //generate a new id
+    var id = new Meteor.Collection.ObjectID();
+
+    Polls.update(
+      { _id: pollID },
+      { $push: { 
+          choices: { id: id._str, text: name, votes: 1 } 
+      } }
+    );
+
+  }
+
+});
