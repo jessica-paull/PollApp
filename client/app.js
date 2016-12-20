b952f08e98457d7881b25fc1d8897095a9217e32
Jessica Paull <jessikwa@live.com>
2016-12-20 17:07:41 -0500

    added theme dropdown

--- client/app.js
+++ client/app.js
@@ -1,59 +1,79 @@
 Meteor.startup(function() {
 
-  Session.set("sort_by", "name");  
+  Session.set('sort_by', 'name');  
 
-  $.getScript("vendor\jquery.countdown.js");
+  $.getScript('vendor\jquery.countdown.js');
 
-  $(".js-countdown").countdown("2016/10/30", function(event) {
+  $('.js-countdown').countdown('2016/12/24 17:00:00', function(event) {
     $(this).text(
-    event.strftime("%D days %H hours %M minutes %S seconds"));
-  }).on("finish.countdown", function() {
-    $(".results-wrapper").show();
-    $(".poll-wrapper").hide();
-    Session.set("sort_by", "results");  
+    event.strftime('%D days %H hours %M minutes %S seconds'));
+  }).on('finish.countdown', function() {
+    $('.results-wrapper').show();
+    $('.poll-wrapper').hide();
+    Session.set('sort_by', 'results');  
   });
+
+  Session.set('pollQuestion', 'Christmas');
 
 }); 
 
 Template.body.helpers({
   
   choices: function() {
-    var sort = Session.get("sort_by");
-    if (sort === "results"){
+    var sort = Session.get('sort_by');
+    if (sort === 'results'){
        return Choices.find({}, {sort: {votes: -1}});
     } else {
        return Choices.find({}, {sort: {text: -1}});
     }
     
+  },
+  pollQuestion: function () {
+    var name = Session.get('pollQuestion'),
+        className = '';
+
+    if (name==='Halloween') {
+      text = 'Who has best costume?';
+      className = 'halloween-body';
+    } else if (name === 'Christmas') {
+      text = 'Who has the ugliest sweater?';
+      className = 'christmas-body';
+    } else {
+      text = 'I have no idea what this poll is for!';
+    }
+
+    $('body').attr('class', '').addClass(className);
+
+    return text;
   }
 
 });
 
 Template.body.events({
 
-  // event to handle adding a person
-  "click .add-person": function(event) {
+  'change .select-mode': function(event) {
+    var input = $('.select-mode'),
+        name = input.val();
 
-    // prevent the default behavior
+    Session.set('pollQuestion', name);
+  },
+
+  'click .add-person': function(event) {
     event.preventDefault();
-  
-    // get the text for the new choice
-    var input = $("input.new-person");
+
+    var input = $('input.new-person');
     var name = input.val();
 
     if (name && name.length) {
+      $('#confirm-vote').modal({'show': true});
 
-      $("#confirm-vote").modal({"show": true});
-
-      input.val("");
+      input.val('');
 
       Choices.insert({
         text: name,
         votes: 1
       });
-
     }
-    
   }
-  
+
 });
